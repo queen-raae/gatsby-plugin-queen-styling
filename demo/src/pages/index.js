161f17793dbbe1f978fb6c0adebadd1e60dc8d1d
Benedicte Raae <raae@raae.codes>
2022-01-11 10:39:41 +0100

    fix: show ordered list numbers

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -14,6 +14,25 @@ const IndexPage = () => {
           </span>
         </h1>
         <p>Amazing plugin that does all the things</p>
+        <ol>
+          <li>
+            List thing <code>one</code>
+          </li>
+          <li>Second thing to be listed</li>
+          <li>
+            There is even a <code>third thing</code>, who would have thunk.
+          </li>
+        </ol>
+
+        <ul>
+          <li>
+            List thing <code>one</code>
+          </li>
+          <li>Second thing to be listed</li>
+          <li>
+            There is even a <code>third thing</code>, who would have thunk.
+          </li>
+        </ul>
       </header>
       <section>
         <form>
