ff6a93ab54ff0b425d0a9dc0df55d3b782d41a15
Benedicte Raae <raae@raae.codes>
2022-06-09 17:55:52 +0200

    feat: only styling, no md setup

--- plugin/gatsby-config.js
+++ plugin/gatsby-config.js
@@ -1,23 +1,3 @@
 module.exports = {
-  plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-prismjs`,
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 800,
-              linkImagesToOriginal: false,
-            },
-          },
-        ],
-      },
-    },
-  ],
+  plugins: [`gatsby-plugin-sass`],
 };
